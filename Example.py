9daa05f4a1a4d5739abc4108070f7397221dc004
driv <federiconafria@gmail.com>
2017-03-23 21:43:58 +0100

    Adding plotting example and readme

---
+++ Example.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import EventProfiler
+import pandas as pd
+
+
+def makeSignal():
+    columns = ['symbol1', 'symbol2']
+    signal = [[False, False],
+              [True, True],
+              [False, True],
+              [True, False],
+              [False, True],
+              [False, False]]
+    index = range(0, len(signal))
+    dataframe = pd.DataFrame(data=signal, index=index, columns=columns)
+    return dataframe
+
+
+def makeData():
+    columns = ['symbol1', 'symbol2']
+    prices = [[-1, 0],
+              [2, 1],
+              [3, 2],
+              [4, 10],
+              [5, 8],
+              [6, 12]]
+    index = range(0, len(prices))
+    dataframe = pd.DataFrame(data=prices, index=index, columns=columns)
+    return dataframe
+
+
+def main():
+    lookback = 1
+    lookforward = 2
+    result = EventProfiler.eventprofiler(
+        makeData(), makeSignal(), lookback, lookforward)
+
+    plt.clf()
+    plt.plot(result['mean'])
+    plt.errorbar(result['mean'].index, result['mean'],
+                 yerr=result['std'], ecolor='#AAAAFF')
+    plt.xlim(-lookback - 1, lookforward + 1)
+    plt.savefig('test.pdf', format='pdf')
+
+if __name__ == '__main__':
+    main()
---
+++ README.md
@@ -0,0 +1,5 @@
+Event profiler based on the one in [QSTK](http://wiki.quantsoftware.org/index.php?title=QSTK_Tutorial_9).
+
+It uses a pandas DataFrame for the symbol values with any index and matches it to a signals DataFrame by the symbol name and index.
+
+Please check the [example](Exammple.py).
